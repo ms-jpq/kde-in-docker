a7dde277bcc99037fb3d7fb7cd6ad21f5d3ddbdc
Fly me to the moon <git@bigly.dog>
2020-02-18 20:28:28 -0800

    push konsole

--- base/bionic/root/etc/cont-init.d/00-desktop.sh
+++ base/bionic/root/etc/cont-init.d/00-desktop.sh
@@ -2,3 +2,4 @@
 
 mkdir -p /root/Desktop
 cat /usr/share/applications/nautilus.desktop > /root/Desktop/nautilus.desktop
+cat /usr/share/applications/org.kde.konsole.desktop > /root/Desktop/konsole.desktop
