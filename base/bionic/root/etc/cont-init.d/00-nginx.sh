c25cc4acfebb2632824e2c22210350c319da8bdf
Fly me to the moon <git@bigly.dog>
2020-02-19 01:45:47 -0800

    rewrite js

--- base/bionic/root/etc/cont-init.d/00-nginx.sh
+++ base/bionic/root/etc/cont-init.d/00-nginx.sh
@@ -11,6 +11,9 @@ fi
 TEMP="$(envsubst '${PATH_PREFIX},${SOCKET_PREFIX}' < /etc/nginx/nginx.conf)"
 echo "$TEMP" > /etc/nginx/nginx.conf
 
-# UI.initSetting('path', 'websockify'); -> UI.initSetting('path', 'prefix/websockify');
 PURE_PREFIX="${SOCKET_PREFIX:1}/"
+# UI.initSetting('path', 'websockify'); -> UI.initSetting('path', 'prefix/websockify');
 sed -i 's@UI.initSetting('\''path'\'', '\''websockify'\'');@UI.initSetting('\''path'\'', '\'''"$PURE_PREFIX"'websockify'\'');@' /novnc/app/ui.js
+
+# readQueryVariable('path', 'websockify'); -> readQueryVariable('path', 'prefix/websockify');
+sed -i 's@readQueryVariable('\''path'\'', '\''websockify'\'');@readQueryVariable('\''path'\'', '\'''"$PURE_PREFIX"'websockify'\'');@' /novnc/vnc_lite.html
