ef06d8f10af4a0cfe23c72788a3c24c6158b6bed
Fly me to the moon <git@bigly.dog>
2020-02-18 22:39:22 -0800

    page title

--- base/bionic/root/etc/cont-init.d/00-page-title.sh
+++ base/bionic/root/etc/cont-init.d/00-page-title.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/with-contenv sh
+
+
+sed -i 's@<title>noVNC<\/title>@''<title>'"$PAGE_TITLE"'<\/title>@' /novnc/vnc.html
+sed -i 's@<title>noVNC<\/title>@''<title>'"$PAGE_TITLE"'<\/title>@' /novnc/vnc_lite.html
