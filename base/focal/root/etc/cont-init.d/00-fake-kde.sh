02405fbc16ac7e35e14dc44db2f9e119df080f7e
Fly me to the moon <git@bigly.dog>
2020-03-08 14:49:37 -0700

    add desktop icons to wine

--- base/focal/root/etc/cont-init.d/00-fake-kde.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eu
-set -o pipefail
-
-chmod +x /usr/lib/x86_64-linux-gnu/libexec/kf5/start_kdeinit_wrapper
---
+++ wine/_root/etc/cont-init.d/02-desktop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+mv /misc/wine.desktop /root/Desktop/wine.desktop
