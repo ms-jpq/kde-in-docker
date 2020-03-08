e3944c69741827d055ced674b463822d59ff3f0d
Fly me to the moon <git@bigly.dog>
2020-03-08 16:39:53 -0700

    wine add explorer

--- wine/_root/etc/cont-init.d/02-desktop.sh
+++ wine/_root/etc/cont-init.d/02-desktop.sh
@@ -3,4 +3,6 @@
 set -eu
 set -o pipefail
 
-mv /misc/wine.desktop /root/Desktop/wine.desktop
+mv /misc/winetricks.desktop /root/Desktop/winetricks.desktop
+mv /misc/explorer.desktop /root/Desktop/explorer.desktop
+mv /misc/ie.desktop /root/Desktop/ie.desktop
